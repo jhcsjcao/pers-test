2ae16bc0295d122a9367450ac79074504c297796
jhcsjcao <jameshychang@yahoo.com>
2026-07-27 17:46:05 -0700

    Add space before Begin Assessment button

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ocean-assessment-v1.00.48';
+const CACHE = 'ocean-assessment-v1.00.49';
 const isDev = self.location.hostname === 'localhost' || self.location.hostname === '127.0.0.1';
 
 self.addEventListener('install', e => {
