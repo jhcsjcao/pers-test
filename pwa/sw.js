5989904a758e88eb79fb681e2f054a5226fedfb2
jhcsjcao <jameshychang@yahoo.com>
2026-07-27 21:43:16 -0700

    Reset advancing flag before calculateResults

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ocean-assessment-v1.00.74';
+const CACHE = 'ocean-assessment-v1.00.75';
 const isDev = self.location.hostname === 'localhost' || self.location.hostname === '127.0.0.1';
 
 self.addEventListener('install', e => {
